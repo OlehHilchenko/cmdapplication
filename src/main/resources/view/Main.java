6a8c10c2234c09688130ce9ecd08d3d23c1f3b7a
Oleh Hilchenko <olehhilchenko@gmail.com>
2019-12-04 17:30:45 +0200

    ninth commit

--- src/main/resources/view/Main.java
+++ src/main/resources/view/Main.java
@@ -9,9 +9,14 @@ import main.resources.repository.ViewSkills;
 public class Main {
 
 
-
     public static void main(String[] args) {
+        Skill skill = new Skill();
+        ViewSkills viewSkills = new ViewSkills();
+        String str [] = viewSkills.viewSkill(222333444666l);
+        skill.setID(Long.parseLong(str[0]));
+        skill.setName(str[1]);
 
+        System.out.println("ID " + skill.getID() + " " + "Name "+ skill.getName());
 
     }
 }
